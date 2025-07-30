79d1da8dae3dc6cf6998edea2408c076785f7a68
nouralrahman Hussain <nouralrahman.hussain@stud.htwk-leipzig.de>
2025-07-30 09:29:59 +0200

    SWS tabelle testimg und optimiern zum speichern

--- rdf-formular/zuarbeitsblatt.js
+++ rdf-formular/zuarbeitsblatt.js
@@ -24,52 +24,17 @@ function saveZuarbeitsblatt(data) {
   ex:modulbezeichnung "${data.modulbezeichnung || ""}" ;
   ex:teilmodulnummer "${data.teilmodulnummer || ""}" ;
   ex:teilmodulbezeichnung "${data.teilmodulbezeichnung || ""}" ;
-  ex:fachart "${data.fachart || ""}"`;
-
-  // ========== 1. Dynamische Gesamt-SWS-Einträge ==========
-  const gesamtSwsEintraege = Object.keys(data)
-    .filter(key => key.startsWith('sws_bezeichnung_') && data[key] !== "")
-    .map(key => {
-      const nr = key.replace('sws_bezeichnung_', '');
-      return {
-        nr,
-        bezeichnung: data[`sws_bezeichnung_${nr}`],
-        sws_v: data[`sws_v_${nr}`] || 0,
-        sws_s: data[`sws_s_${nr}`] || 0,
-        sws_p: data[`sws_p_${nr}`] || 0,
-        raum: data[`sws_raum_${nr}`] || "",
-        technik: data[`sws_technik_${nr}`] || "",
-        campus: data[`sws_campus_${nr}`] || ""
-      };
-    })
-    .sort((a, b) => parseInt(a.nr) - parseInt(b.nr));
-
-  let swsTriples = '';
-  let swsRefs = [];
-
-  gesamtSwsEintraege.forEach((eintrag, index) => {
-    const sid = `${id}_sws${eintrag.nr}`;
-    swsRefs.push(`:GesamtSWS_${sid}`);
-    swsTriples += `
-:GesamtSWS_${sid} a ex:GesamtSWS ;
-  ex:lfd "${index + 1}" ;
-  ex:bezeichnung "${eintrag.bezeichnung}" ;
-  ex:swsV ${eintrag.sws_v} ;
-  ex:swsS ${eintrag.sws_s} ;
-  ex:swsP ${eintrag.sws_p} ;
-  ex:raum "${eintrag.raum}" ;
-  ex:technik "${eintrag.technik}" ;
-  ex:campus "${eintrag.campus}" ;
-  ex:zugeordnetZu :Zuarbeitsblatt_${id} .\n`;
-  });
-
-  if (swsRefs.length > 0) {
-    turtle += ` ;
-  ex:gesamtSWS ${swsRefs.join(', ')}`;
-  }
-
-  turtle += " .\n";
-  turtle += swsTriples;
+  ex:fachart "${data.fachart || ""}" ;
+  ex:swsV ${data.sws_v || 0} ;
+  ex:swsS ${data.sws_s || 0} ;
+  ex:swsP ${data.sws_p || 0} ;
+  ex:raumV "${data.raum_v || ""}" ;
+  ex:raumS "${data.raum_s || ""}" ;
+  ex:raumP "${data.raum_p || ""}" ;
+  ex:technikV "${data.technik_v || ""}" ;
+  ex:technikS "${data.technik_s || ""}" ;
+  ex:technikP "${data.technik_p || ""}"
+.`;
 
   // ========== 2. Datei schreiben ==========
   const dirPath = path.join(__dirname, 'data');
